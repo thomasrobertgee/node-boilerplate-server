30020be65f2db5a7d9b128630157ec200a828cb4
Thomas Gee <thomasrobertgee@gmail.com>
2017-03-05 12:19:39 +1100

    new user can now be saved to the db via Postman

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -8,14 +8,38 @@ exports.signup = function(req, res, next) {
 
   // see if a user with the given email exists
   User.findOne({ email: email }, function(err, existingUser) {
+    if (err) { return next(err)}
 
+    // if a user with email does exist, return an error
+    if (existingUser) {
+      return res.status(422).send({ error: 'Email already exists' })
+    }
+
+    // if a user with a fresh email, create and save user record
+    const user = new User({
+      email: email,
+      password: password
+    })
+
+    // save user
+    user.save(function(err) {
+      // if there is an error when saving user
+      if (err) { return next(err) }
+
+      // respond to request indicating the user was created
+      res.json({ success: true})
+      // note that an id for the user is automaticaly created when the user is saved
+    })
   })
-
-  // if a user with email does exist, return an error
-
-
-  // if a user with a fresh email, create and save user record
-
-
-  // respond to request indicating the user was created
 }
+
+// if all is successful, Postman should spit back something like this:
+// {
+//   "success": true
+// }
+
+// if you send the request a second time, Postman should spit back this:
+// {
+//   "error": "Email already exists"
+// }
+// only if the email was the same in both requests
