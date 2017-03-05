33fa1268cea389c6221a3aa159f57767a1621e61
Thomas Gee <thomasrobertgee@gmail.com>
2017-03-05 12:34:39 +1100

    passwords are now hashed

--- models/user.js
+++ models/user.js
@@ -1,11 +1,35 @@
 const mongoose = require('mongoose')
 // Schema is what we use to tell mongoose what particular fields our model is going to have
 const Schema = mongoose.Schema
+const bcrypt = require('bcrypt-nodejs')
 
 // Define our model
 const userSchema = new Schema({
   email: { type: String, unique: true, lowercase: true }, // ensures unique emails are all lowercase to avoid duplication
   password: String
+})
+
+// On Save Hook, encrypt password
+// before saving a model, run this function
+userSchema.pre('save', function(next) {
+  // get access to the user model
+  const user = this
+
+  // generate a salt then run callback
+  bcrypt.genSalt(10, function(err, salt) {
+    if (err) { return next(err) }
+
+    // hash our password using the salt
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if (err) { return next(err) }
+
+      // overwrite plain text password with hashed password
+      user.password = hash
+      
+      // save and go forth
+      next()
+    })
+  })
 })
 
 
