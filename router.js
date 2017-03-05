3800b6fe23e873c0d6341f31633325cffcb58447
Thomas Gee <thomasrobertgee@gmail.com>
2017-03-05 15:07:12 +1100

    added authorization with JWT tokens and middleware

--- router.js
+++ router.js
@@ -1,9 +1,18 @@
 const Authentication = require('./controllers/authentication')
+const passportService = require('./services/passport')
+const passport = require('passport')
+
+// requireAuth is the middleware
+const requireAuth = passport.authenticate('jwt', { session: false }) // we don't want cookies to be created by default because we are using jwt tokens so flase disbales the creation of cookies
 
 module.exports = function(app) {
   // req = request which is an object that represents the incoming http request (holds the request data)
   // res = response object allows us to respond to our user in some way
   // next is mostly for error handling
+
+  app.get('/', requireAuth, function(req, res) {
+    res.send({ hi: 'there' })
+  })
 
 
   // if the user posts anything to /signup then run this function
